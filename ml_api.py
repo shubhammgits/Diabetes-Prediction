0a8868932ca8f2f9e6975655e0e6fc5d0e373a9a
shubhammgits <shubhamm18.work@gmail.com>
2025-10-03 00:10:16 +0530

    params

--- ml_api.py
+++ ml_api.py
@@ -2,3 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import json
+
+app = FastAPI()
+
+class model_input(BaseModel):
+    Pregnancies : int
+    Glucose : int
+    BloodPressure : int
+    SkinThickness : int
+    Insulin : int
+    BMI : float
+    DiabetesPedigreeFunction : float
+    Age : int
